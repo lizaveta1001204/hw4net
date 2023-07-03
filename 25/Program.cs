07a822b6e6570137e8d2edd801a0f33ad9f8df54
liza <lizaveta2011@tut.by>
2023-07-03 11:19:53 +0300

    25

---
+++ 25/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+
+  Console.Write("Введите число   : ");
+  int num1 = int.Parse(Console.ReadLine()!);
+  Console.Write("Введите степень : ");
+  int num2 = int.Parse(Console.ReadLine())!;
+int Exponentiation(int num1, int num2)
+{
+  int result = 1;
+  for(int i=1; i <= num2; i++){
+    result = result * num1;
+  }
+    return result;
+}
+int exponentiation = Exponentiation(num1, num2);
+  Console.WriteLine("Ответ: " + exponentiation);
