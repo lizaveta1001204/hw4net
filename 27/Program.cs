7567fe87b5ac1bc232b9274a0152f9583bc9905d
liza <lizaveta2011@tut.by>
2023-07-03 11:30:16 +0300

    27

---
+++ 27/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int GetSum(int n)
+{
+    int sum = 0;
+    sum = NewMethod(n, sum);
+    bool i = false;
+    Console.WriteLine(i);
+    return sum;
+
+    static int NewMethod(int n, int sum)
+    {
+        for (int i = 1; i <= n; i++)
+            sum += i;
+        return sum;
+    }
+}
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+int result = GetSum(n);
+Console.WriteLine($"Сумма от 1 до {n} равна {result}");
+
